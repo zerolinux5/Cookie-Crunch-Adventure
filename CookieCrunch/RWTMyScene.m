a24167a5eb06e3adbb648fc4866a376f4ff6e576
Jesus Magana <maganazero5@gmail.com>
2014-07-07 10:14:05 -0700

    Can now swap

--- CookieCrunch/RWTMyScene.m
+++ CookieCrunch/RWTMyScene.m
@@ -19,6 +19,9 @@ static const CGFloat TileHeight = 36.0;
 @property (strong, nonatomic) SKNode *cookiesLayer;
 @property (strong, nonatomic) SKNode *tilesLayer;
 
+@property (assign, nonatomic) NSInteger swipeFromColumn;
+@property (assign, nonatomic) NSInteger swipeFromRow;
+
 @end
 
 @implementation RWTMyScene
@@ -44,6 +47,8 @@ static const CGFloat TileHeight = 36.0;
         [self.gameLayer addChild:self.tilesLayer];
         
         [self.gameLayer addChild:self.cookiesLayer];
+        
+        self.swipeFromColumn = self.swipeFromRow = NSNotFound;
     }
     return self;
 }
@@ -61,6 +66,26 @@ static const CGFloat TileHeight = 36.0;
     return CGPointMake(column*TileWidth + TileWidth/2, row*TileHeight + TileHeight/2);
 }
 
+- (BOOL)convertPoint:(CGPoint)point toColumn:(NSInteger *)column row:(NSInteger *)row {
+    NSParameterAssert(column);
+    NSParameterAssert(row);
+    
+    // Is this a valid location within the cookies layer? If yes,
+    // calculate the corresponding row and column numbers.
+    if (point.x >= 0 && point.x < NumColumns*TileWidth &&
+        point.y >= 0 && point.y < NumRows*TileHeight) {
+        
+        *column = point.x / TileWidth;
+        *row = point.y / TileHeight;
+        return YES;
+        
+    } else {
+        *column = NSNotFound;  // invalid location
+        *row = NSNotFound;
+        return NO;
+    }
+}
+
 - (void)addTiles {
     for (NSInteger row = 0; row < NumRows; row++) {
         for (NSInteger column = 0; column < NumColumns; column++) {
@@ -71,6 +96,85 @@ static const CGFloat TileHeight = 36.0;
             }
         }
     }
+}
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    // 1
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInNode:self.cookiesLayer];
+    
+    // 2
+    NSInteger column, row;
+    if ([self convertPoint:location toColumn:&column row:&row]) {
+        
+        // 3
+        RWTCookie *cookie = [self.level cookieAtColumn:column row:row];
+        if (cookie != nil) {
+            
+            // 4
+            self.swipeFromColumn = column;
+            self.swipeFromRow = row;
+        }
+    }
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    // 1
+    if (self.swipeFromColumn == NSNotFound) return;
+    
+    // 2
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInNode:self.cookiesLayer];
+    
+    NSInteger column, row;
+    if ([self convertPoint:location toColumn:&column row:&row]) {
+        
+        // 3
+        NSInteger horzDelta = 0, vertDelta = 0;
+        if (column < self.swipeFromColumn) {          // swipe left
+            horzDelta = -1;
+        } else if (column > self.swipeFromColumn) {   // swipe right
+            horzDelta = 1;
+        } else if (row < self.swipeFromRow) {         // swipe down
+            vertDelta = -1;
+        } else if (row > self.swipeFromRow) {         // swipe up
+            vertDelta = 1;
+        }
+        
+        // 4
+        if (horzDelta != 0 || vertDelta != 0) {
+            [self trySwapHorizontal:horzDelta vertical:vertDelta];
+            
+            // 5
+            self.swipeFromColumn = NSNotFound;
+        }
+    }
+}
+
+- (void)trySwapHorizontal:(NSInteger)horzDelta vertical:(NSInteger)vertDelta {
+    // 1
+    NSInteger toColumn = self.swipeFromColumn + horzDelta;
+    NSInteger toRow = self.swipeFromRow + vertDelta;
+    
+    // 2
+    if (toColumn < 0 || toColumn >= NumColumns) return;
+    if (toRow < 0 || toRow >= NumRows) return;
+    
+    // 3
+    RWTCookie *toCookie = [self.level cookieAtColumn:toColumn row:toRow];
+    if (toCookie == nil) return;
+    
+    // 4
+    RWTCookie *fromCookie = [self.level cookieAtColumn:self.swipeFromColumn row:self.swipeFromRow];
+    
+    NSLog(@"*** swapping %@ with %@", fromCookie, toCookie);
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    self.swipeFromColumn = self.swipeFromRow = NSNotFound;
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self touchesEnded:touches withEvent:event];
 }
 
 @end
